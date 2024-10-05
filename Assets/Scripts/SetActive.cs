0e5954505b8f84639848c172c0592823fac137d1
Andres <andresaleksandarmilutin@gmail.com>
2024-10-05 05:10:25 -0500

    GUI-Updated
    GUI updated -> Settings button window works with a carousell panel, clicks on arrows to navigate the images, press close to close the settings manual window.
    
    Settings window hace el fondo del juego mas oscuro para enfocar los ojos del jugador en la pantalla del carousell
    
    Also+ -> Added a prefab and a Script for the active settings window.

---
+++ Assets/Scripts/SetActive.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveObject : MonoBehaviour
+{
+    public GameObject activeGameObject;
+
+
+    public void ActivateObject()
+    {
+        if (activeGameObject.activeSelf != true)
+        {
+            activeGameObject.SetActive(true);
+        }
+        else
+        {
+            activeGameObject.SetActive(false);
+  
+        }
+    }
+}
