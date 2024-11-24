a233b95a4ff55a7438edf3ef2de7850fa4f73194
Tomas Mileto <tomasmileto@gmail.com>
2024-11-24 17:36:11 -0300

    Add weighted list generic functionality and update CardRewardData.cs

--- Assets/Tools/NueDeck/Scripts/Data/Collection/RewardData/CardRewardData.cs
+++ Assets/Tools/NueDeck/Scripts/Data/Collection/RewardData/CardRewardData.cs
@@ -1,12 +1,81 @@
-﻿using System.Collections.Generic;
+﻿using NaughtyAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 namespace NueGames.NueDeck.Scripts.Data.Collection.RewardData
 {
-    [CreateAssetMenu(fileName = "Card Reward Data",menuName = "NueDeck/Collection/Rewards/CardRW",order = 0)]
+    [CreateAssetMenu(fileName = "Card Reward Data", menuName = "NueDeck/Collection/Rewards/CardRW", order = 0)]
     public class CardRewardData : RewardDataBase
     {
         [SerializeField] private List<CardData> rewardCardList;
+
         public List<CardData> RewardCardList => rewardCardList;
+
+        public WeightedList<CardData> weightedCardRewards;
+
+        [SerializeField] List<(int, CardData)> cardWeights;
     }
+
+    [Serializable]
+    public class WeightedList<T> : ISerializationCallbackReceiver
+    {
+        [AllowNesting, SerializeField, ReadOnly, Label("Total Weight")]
+        private int InspectorTotalWeight = 0;
+
+        public List<WeightedItem<T>> Items;
+
+        public int TotalWeight { get => Items.Sum(i => i.Weight);}
+
+
+        void OnValidate()
+        {
+            UpdateInspectorPercentages();
+        }
+        void ISerializationCallbackReceiver.OnBeforeSerialize() => this.OnValidate();
+        void ISerializationCallbackReceiver.OnAfterDeserialize() => this.UpdateInspectorPercentages();
+
+        public void UpdateInspectorPercentages()
+        {
+            InspectorTotalWeight = TotalWeight;
+
+            if (TotalWeight > 0)
+            {
+                foreach (var item in Items)
+                {
+                    item.PercentInList = $" ({((float)item.Weight / TotalWeight * 100).ToString("##")}%)";
+                }
+            }
+        }
+    }
+
+    [Serializable]
+    public class WeightedItem<T> : ISerializationCallbackReceiver
+    {
+        [HideInInspector] public string Name;
+        [HideInInspector] public string PercentInList;
+
+        public int Weight;
+        public T Item;
+
+        void OnValidate()
+        {
+            if (Item is UnityEngine.Object obj)
+                Name = $"{obj.name}";
+            else
+                Name = $"{Item.ToString()}";
+
+            Name += $".     [W:{Weight}]{PercentInList}";
+
+        }
+        void ISerializationCallbackReceiver.OnBeforeSerialize() => this.OnValidate();
+        void ISerializationCallbackReceiver.OnAfterDeserialize() { }
+
+    }
+
+
+
 }
