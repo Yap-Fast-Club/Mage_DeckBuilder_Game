3f4473bc8745d7696924c5b7a4dd33079ff734ca
Tomas Mileto <tomasmileto@gmail.com>
2024-11-27 19:34:06 -0300

    Make Food for Handell erase only common cards

--- Assets/Tools/NueDeck/Scripts/Card/CardActions/RandomEraseCardAction.cs
+++ Assets/Tools/NueDeck/Scripts/Card/CardActions/RandomEraseCardAction.cs
@@ -13,14 +13,32 @@ namespace NueGames.NueDeck.Scripts.Card.CardActions
 
         public override void DoAction(CardActionParameters actionParameters)
         {
+            var targetRarity = actionParameters.AreaValue switch
+            {
+                1 => RarityType.Common,
+                2 => RarityType.Rare,
+                3 => RarityType.Legendary,
+                _ => RarityType.Common
+            };
+
+
+
             if (CollectionManager.DrawPile.Count > 0)
             {
                 var randomCardData = CollectionManager.DrawPile.RandomItem();
                 CollectionManager.DrawCard(randomCardData);
 
-                var cardInHand = CollectionManager.HandController.hand.Find(c => c.CardData == randomCardData);
+                CardBase cardInHand=null;
+                if (actionParameters.AreaValue == 0)
+                    cardInHand = CollectionManager.HandController.hand.Find(c => c.CardData == randomCardData);
+                else if (actionParameters.AreaValue <= 3)
+                    cardInHand = CollectionManager.HandController.hand.Find(c => c.CardData == randomCardData && c.CardData.Rarity == targetRarity);
+
+                if (cardInHand == null)
+                    return;
+
                 CollectionManager.HandController.StopAllCoroutines();
-                CollectionManager.HandController.RemoveCardFromHand(0);
+                CollectionManager.HandController.RemoveCardFromHand(cardInHand.CardData);
 
                 cardInHand.Exhaust(false);
             }
