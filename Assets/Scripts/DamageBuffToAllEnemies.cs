9d21c6fc0e31cc3677a43a446cb7dc49c792f5e6
Tomas Mileto <tomasmileto@gmail.com>
2024-12-06 19:08:19 -0300

    Fix some bugs with MiniBoss passives

--- Assets/Scripts/DamageBuffToAllEnemies.cs
+++ Assets/Scripts/DamageBuffToAllEnemies.cs
@@ -13,14 +13,21 @@ public class DamageBuffToAllEnemies : MonoBehaviour
 
     void Start()
     {
+        //apply buff to already present enemies
+        CombatManager.Instance.CurrentEnemiesList.ForEach(e =>
+        {
+            if (e == this.GetComponent<EnemyBase>()) return;
+
+            GiveBuff(e);
+        });
         CombatManager.Instance.EnemySpawned += GiveBuff;
         CombatManager.Instance.EnemyDeath += RemoveBuff;
     }
 
     private void OnDisable()
     {
-        CombatManager.Instance.EnemySpawned += GiveBuff;
-        CombatManager.Instance.EnemyDeath += RemoveBuff;
+        CombatManager.Instance.EnemySpawned -= GiveBuff;
+        CombatManager.Instance.EnemyDeath -= RemoveBuff;
 
         for (int i = _currentAffectedEnemies.Count - 1; i >= 0; i--)
         {
@@ -36,7 +43,7 @@ public class DamageBuffToAllEnemies : MonoBehaviour
 
     private void RemoveBuff(EnemyBase enemy)
     {
-        enemy.CharacterStats.SetCurrentHealth(enemy.CharacterStats.CurrentDamage - damageToGive);
+        enemy.CharacterStats.SetCurrentDamage(enemy.CharacterStats.CurrentDamage - damageToGive);
         _currentAffectedEnemies.Remove(enemy);
     }
 
--- Assets/Scripts/MaxHealthBuffToAllEnemies.cs
+++ Assets/Scripts/MaxHealthBuffToAllEnemies.cs
@@ -13,14 +13,22 @@ public class MaxHealthBuffToAllEnemies : MonoBehaviour
 
     void Start()
     {
+        //apply buff to already present enemies
+        CombatManager.Instance.CurrentEnemiesList.ForEach(e =>
+        {
+            if (e == this.GetComponent<EnemyBase>()) return;
+
+            GiveBuff(e);
+        });
+
         CombatManager.Instance.EnemySpawned += GiveBuff;
         CombatManager.Instance.EnemyDeath += RemoveBuff;
     }
 
     private void OnDisable()
     {
-        CombatManager.Instance.EnemySpawned += GiveBuff;
-        CombatManager.Instance.EnemyDeath += RemoveBuff;
+        CombatManager.Instance.EnemySpawned -= GiveBuff;
+        CombatManager.Instance.EnemyDeath -= RemoveBuff;
 
         for (int i = _currentAffectedEnemies.Count - 1; i >= 0; i--)
         {
